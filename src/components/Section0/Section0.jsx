143c991f4a4f386a4571bc3e4e27e52244647baa
Troy Pernía <troy00pernia@gmail.com>
2023-08-15 16:12:03 -0400

    change components/Section0: add new information (line 10-56)

--- src/components/Section0/Section0.jsx
+++ src/components/Section0/Section0.jsx
@@ -7,12 +7,53 @@ import style from './Section0.module.css'
 
 import data from '../../../troy.json'
 
-const { news, map, noteBook } = data
+const { news, map, noteBook, homeHeader } = data
+
+function convertSize (input) {
+  const n = Number(input.split('px')[0])
+  return (n * 100 / 1920) + 'vw'
+}
 
 function Section0 () {
+  const homeHeaderContainer = {
+    height: convertSize(homeHeader.size.height)
+  }
+
+  const homeHeaderFrontPageStyle = {
+    height: convertSize(homeHeader.image.height)
+  }
+
+  const textStyles = homeHeader.text.map((texto, index) => ({
+    ...texto,
+    flag: '',
+    content: '',
+    top: convertSize(texto.top),
+    left: convertSize(texto.left),
+    width: convertSize(texto.width),
+    height: convertSize(texto.height),
+    fontSize: convertSize(texto.fontSize),
+    lineHeight: texto.lineHeight && convertSize(texto.lineHeight)
+  }))
+
   return (
-    <section className={style.Section0Background}>
-      TRATA DE PERSONAS
+    <section className={style.Section0Background} style={homeHeaderContainer}>
+      <div style={homeHeaderFrontPageStyle} className={style.Section0HomeHeader} />
+      {
+        homeHeader.text.map((texto, index) => {
+          switch (texto.tag) {
+            case 'h1': {
+              return <h1 className={style.HomeHeaderText} style={textStyles[index]} key={index}>{texto.content}</h1>
+            }
+            case 'h2': {
+              return <h2 className={style.HomeHeaderText} style={textStyles[index]} key={index}>{texto.content}</h2>
+            }
+            case 'p': {
+              return <p className={style.HomeHeaderText} style={textStyles[index]} key={index}>{texto.content}</p>
+            }
+          }
+          return true
+        })
+      }
       <NoteBook noteBook={noteBook} />
       <News news={news} />
       <Map map={map} />
