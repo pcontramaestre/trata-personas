251d8ca79fb4eeb51c48ef703bc5c10166c8fa13
Troy Pernía <troy00pernia@gmail.com>
2023-08-21 10:46:03 -0400

    change components/Section4: add function "convertSize" (line 11-15) and const "lawEnforcementeBackground" (line 18-22)

--- src/components/Section4/Section4.jsx
+++ src/components/Section4/Section4.jsx
@@ -4,10 +4,25 @@ import SmallInfoBox from '../SmallInfoBox/SmallInfoBox'
 
 import style from './Section4.module.css'
 
+import data from '../../../troy.json'
+
+const { lawEnforcement } = data
+
+function convertSize (input) {
+  const n = Number(input.split('px')[0])
+  const r = (n * 100 / 1920) + 'vw'
+  return r
+}
+
 function Section4 () {
+  const lawEnforcementeBackground = {
+    width: convertSize(lawEnforcement.width),
+    height: convertSize(lawEnforcement.height),
+    backgroundColor: lawEnforcement.backgroundColor
+  }
+
   return (
-    <section className={style.Section4}>
-      PROCURACIÓN DE LA JUSTICIA
+    <section className={style.LawEnforcementBackground} style={lawEnforcementeBackground}>
       <BarGraph />
       <SmallInfoBox />
       <News />
