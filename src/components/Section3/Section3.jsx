8fa7914b96f194595d02859e1716641c84b518c2
Troy Pernía <troy00pernia@gmail.com>
2023-08-22 17:31:03 -0400

    change components/Section3: add useEffect (line 61-112) and the component BigCard (line 184-251)

--- src/components/Section3/Section3.jsx
+++ src/components/Section3/Section3.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 import NoteBook from '../NoteBook/NoteBook'
 // import BarGraph from '../BarGraph/BarGraph'
 import SmallInfoBox from '../SmallInfoBox/SmallInfoBox'
@@ -15,6 +17,14 @@ import atenciones from '../../assets/Section3/atenciones.svg'
 import group from '../../assets/Instructions/group.svg'
 import infoGif from '../../assets/Instructions/infoGifBlanco1.svg'
 import audio from '../../assets/Instructions/audio1.svg'
+import rows from '../../assets/Section3/rows.png'
+import psicosocial from '../../assets/Section3/psicosocial.svg'
+import medico from '../../assets/Section3/medico.svg'
+import legal from '../../assets/Section3/legal.svg'
+import manutencion from '../../assets/Section3/manutencion.svg'
+import educacion from '../../assets/Section3/educacion.svg'
+import alojamiento from '../../assets/Section3/alojamiento.svg'
+import reintegracion from '../../assets/Section3/reintegracion.svg'
 
 const imagesList = {
   frontPage,
@@ -23,7 +33,15 @@ const imagesList = {
   frecuencia,
   atenciones,
   group,
-  audio
+  audio,
+  rows,
+  psicosocial,
+  medico,
+  legal,
+  manutencion,
+  educacion,
+  alojamiento,
+  reintegracion
 }
 
 const iconList = {
@@ -31,7 +49,7 @@ const iconList = {
 }
 
 const { protectionAndCare } = data
-const { images, text, icons, noteBook, news } = protectionAndCare
+const { images, text, icons, noteBook, news, bigCard } = protectionAndCare
 
 function convertSize (input) {
   const n = Number(input.split('px')[0])
@@ -40,6 +58,58 @@ function convertSize (input) {
 }
 
 function Section3 () {
+  const [nameBigCard, setNameBigCard] = useState('')
+
+  useEffect(() => {
+    // if (bigCard) {
+    //   const icon = document.getElementById('infoGif')
+    //   const div = document.getElementById('SmallInfoBoxinfoGif')
+
+    //   function activeDisplay () {
+    //     div.style.display = 'flex'
+    //   }
+
+    //   function desactiveDisplay () {
+    //     div.style.display = ''
+    //   }
+
+    //   icon.addEventListener('mouseenter', activeDisplay)
+    //   div.addEventListener('mouseleave', desactiveDisplay)
+    //   return () => {
+    //     icon.removeEventListener('mouseenter', activeDisplay)
+    //     div.removeEventListener('mouseleave', desactiveDisplay)
+    //   }
+    // }
+  }, [])
+
+  useEffect(() => {
+    if (bigCard) {
+      const images = document.getElementsByName('proteccion')
+      const div = document.getElementById('BigCardProtection')
+
+      function activeDisplay (event) {
+        setNameBigCard(event.target.id)
+        div.style.display = 'block'
+      }
+
+      function desactiveDisplay () {
+        div.style.display = ''
+        setNameBigCard('')
+      }
+
+      images.forEach(image => {
+        image.addEventListener('mouseenter', activeDisplay)
+      })
+      div.addEventListener('mouseleave', desactiveDisplay)
+      return () => {
+        images.forEach(image => {
+          image.removeEventListener('mouseenter', activeDisplay)
+        })
+        div.removeEventListener('mouseleave', desactiveDisplay)
+      }
+    }
+  }, [])
+
   const protectionAndCareBackgroundStyles = {
     width: convertSize(protectionAndCare.width),
     height: convertSize(protectionAndCare.height),
@@ -72,11 +142,13 @@ function Section3 () {
     left: convertSize(icon.left)
   }))
 
+  const listImageHover = ['psicosocial', 'medico', 'legal', 'manutencion', 'educacion', 'alojamiento', 'reintegracion']
+
   return (
     <section className={style.ProtectionCare} style={protectionAndCareBackgroundStyles}>
       {
         images.map((image, index) => (
-          <img src={image && imagesList[image.name]} style={imagesStyles[index]} className={style.ProtectionCareImages} key={image.name + index} />
+          <img id={listImageHover.includes(image.name) ? image.name : null} name={image.grupo ? image.grupo : null} src={image && imagesList[image.name]} style={imagesStyles[index]} className={style.ProtectionCareImages} key={image.name + index} />
         ))
       }
       {
@@ -103,8 +175,77 @@ function Section3 () {
       <NoteBook noteBook={noteBook} topSection={protectionAndCare.top} />
       {/* <BarGraph /> */}
       <SmallInfoBox />
+      <BigCard bigCard={bigCard} topSection={protectionAndCare.top} name={nameBigCard} />
       <News news={news} topSection={protectionAndCare.top} />
     </section>
+  )
+}
+
+function BigCard ({ bigCard, topSection, name }) {
+  console.log('name:', name)
+  console.log('bigCard:', bigCard)
+  if (bigCard[name]) console.log('bigCard[name]:', bigCard[name])
+  const cardStyles = name
+    ? {
+        ...bigCard,
+        width: convertSize(bigCard.width),
+        height: convertSize(bigCard.height),
+        top: convertSize(Number(bigCard[name].top.split('px')[0]) - Number(topSection.split('px')[0]) + 'px'),
+        left: convertSize(bigCard[name].left),
+        backgroundImage: `url("./src/assets/Section3/${bigCard.backgroundImage}.png")`
+      }
+    : {}
+
+  const titleStyles = name
+    ? {
+        ...bigCard.titleStile,
+        fontSize: convertSize(bigCard.titleStile.fontSize),
+        lineHeight: convertSize(bigCard.titleStile.lineHeight),
+        width: convertSize(bigCard[name].title.width),
+        height: convertSize(bigCard[name].title.height),
+        top: convertSize(bigCard[name].title.top),
+        left: convertSize(bigCard[name].title.left)
+      }
+    : {}
+
+  const subtitleStyles = name
+    ? {
+        ...bigCard.subtitleStile,
+        fontSize: convertSize(bigCard.subtitleStile.fontSize),
+        lineHeight: convertSize(bigCard.subtitleStile.lineHeight),
+        width: convertSize(bigCard[name].subtitle.width),
+        height: convertSize(bigCard[name].subtitle.height),
+        top: convertSize(bigCard[name].subtitle.top),
+        left: convertSize(bigCard[name].subtitle.left)
+      }
+    : {}
+
+  const textStyle = name
+    ? {
+        ...bigCard.textStile,
+        fontSize: convertSize(bigCard.textStile.fontSize),
+        lineHeight: convertSize(bigCard.textStile.lineHeight),
+        width: convertSize(bigCard[name].text.width),
+        height: convertSize(bigCard[name].text.height),
+        top: convertSize(bigCard[name].text.top),
+        left: convertSize(bigCard[name].text.left)
+      }
+    : {}
+
+  return (
+    <div style={cardStyles} className={style.BigCardStyles} id='BigCardProtection'>
+      {
+        name
+          ? (
+            <>
+              <h1 style={titleStyles} className={style.textStyles}>{bigCard[name].title.content}</h1>
+              <h2 style={subtitleStyles} className={style.textStyles}>{bigCard[name].subtitle.content}</h2>
+              <p style={textStyle} className={style.textStyles}>{bigCard[name].text.content}</p>
+            </>
+            )
+          : null
+      }
+    </div>
   )
 }
 
