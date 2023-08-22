dcf9763636e5c003122cf566c1e5f564ac9e0fa5
Jessica Grajales <fjessica28-11@hotmail.com>
2023-08-22 10:13:20 -0500

    1. Se ajusta sección 9.

--- src/components/Section9/Components/cuadros/cuadro8.jsx
+++ src/components/Section9/Components/cuadros/cuadro8.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Cuadro8 = () => {
   return (
     <div className="cuadro8-text">
-      <div className="cuadro8-title">
+      <h2 className="cuadro8-title">
         Delincuencia organizada transnacional. La trata de personas: compraventa
         de seres humanos.
-      </div>
+      </h2>
       <p className="cuadro8-description">
         Oficina de las Naciones Unidas contra la Droga y el Delito, UNODC.
         (s.f.).
--- src/components/Section9/Components/cuadros/cuadro9.jsx
+++ src/components/Section9/Components/cuadros/cuadro9.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Cuadro9 = () => {
   return (
     <div className="cuadro9-text">
-      <div className="cuadro9-title">
+      <h2 className="cuadro9-title">
         Día Mundial contra la Trata- 30 de julio.
-      </div>
+      </h2>
       <p className="cuadro9-description">
         Oficina de las Naciones Unidas contra la Droga y el Delito, UNODC.
         (s.f.)
