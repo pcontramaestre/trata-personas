bb944c495c3a0a4a60a5142cc12fd2abad71cb47
adrian4058 <adrian_2016_@outlook.es>
2023-08-23 15:42:01 -0300

    Adopncion, Explotacion y Mendicidad Responsive

--- src/components/Section2/Explotacion/ExplotacionLaboral/ExplotacionLaboral.jsx
+++ src/components/Section2/Explotacion/ExplotacionLaboral/ExplotacionLaboral.jsx
@@ -13,7 +13,10 @@ const ExplotacionLaboral = () => {
   return (
     <div className="explotacion__laboral">
       <div className="explotacion__laboral-content">
-        <h1 className="laboral__title">{page6.title}</h1>
+        <h1
+          className="laboral__title"
+          dangerouslySetInnerHTML={{ __html: page6.title }}
+        />
         <BookLaboral page6={page6} />
       </div>
       {/* <img className="laboral__img" src={backgroundpasto} /> */}
