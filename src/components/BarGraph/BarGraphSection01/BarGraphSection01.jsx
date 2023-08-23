e6a8d242c1cb61069d0179eaef084199a67c9559
Pablo Contramaestre <pablo@icreativadigital.com>
2023-08-23 18:27:44 -0400

    Animacion ejemplo de en GSAP, Instalacion paquete GSAP

--- src/components/BarGraph/BarGraphSection01/BarGraphSection01.jsx
+++ src/components/BarGraph/BarGraphSection01/BarGraphSection01.jsx
@@ -1,13 +1,85 @@
+import React, { useLayoutEffect, useEffect, useRef } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import "./BarGraphSection01.css";
 import data from "../../../../trataSection01.json";
 
 function BarGraphSection01() {
   const page4 = data[0].visibilizacion_victimas.page4;
+
+  const titleRef = useRef();
+  const imageRef = useRef();
+  const textsRef = useRef();
+
+  const barGraphRef = useRef();
+
+  useLayoutEffect(() => {
+    gsap.to(barGraphRef.current, {
+      opacity: 1, // Asegura que el elemento sea visible cuando comience la animación
+      scrollTrigger: {
+        trigger: barGraphRef.current,
+        
+        start: "top center", // Inicia las animaciones cuando el inicio del div está en el centro de la vista
+        end: "bottom center", // Termina las animaciones cuando el final del div está en el centro de la vista
+        scrub: true,
+      },
+    });
+
+    let tl = gsap.timeline({
+      opacity: 1,
+      scrollTrigger: {
+        trigger: barGraphRef.current,
+        start: "top center", // Inicia las animaciones cuando el inicio del div está en el centro de la vista
+        end: "bottom center", // Termina las animaciones cuando el final del div está en el centro de la vista
+        // pin: true,
+        scrub: true,
+        markers: true,
+      },
+    });
+
+    tl.fromTo(
+      titleRef.current,
+      {
+        x: "-100%",
+        opacity: 0,
+      },
+      {
+        x: "0%",
+        opacity: 1,
+      }
+    );
+
+    tl.fromTo(
+      imageRef.current,
+      {
+        y: "100%",
+        opacity: 0,
+      },
+      {
+        y: "0%",
+        opacity: 1,
+      }
+    );
+
+    tl.fromTo(
+      textsRef.current,
+      {
+        x: "100%",
+        opacity: 0,
+      },
+      {
+        x: "0%",
+        opacity: 1,
+      }
+    );
+  }, []);
+
+
   return (
-    <div className="BarGraph">
+    <div className="BarGraph" ref={barGraphRef}>
       <div className="baragraph__graphic">
-        <h2 className="bargraph__title">{page4.graphic.title}</h2>
-        <div className="bargraph__image">
+        <h2 className="bargraph__title"  ref={titleRef}>{page4.graphic.title}</h2>
+        <div className="bargraph__image" ref={imageRef}>
           {" "}
           <div className="baragraphcolumns">
             <ul className="baragraph__columns-1">
@@ -25,7 +97,7 @@ function BarGraphSection01() {
           <li>{page4.graphic.year4}</li>
         </ul>
       </div>
-      <div className="bargraph__texts">
+      <div className="bargraph__texts" ref={textsRef}>
         <p
           className="bargraph__text1"
           dangerouslySetInnerHTML={{ __html: page4.text1 }}
--- src/components/NoteBook/NoteBookSection01/NoteBookSection01.jsx
+++ src/components/NoteBook/NoteBookSection01/NoteBookSection01.jsx
@@ -1,23 +1,77 @@
+import React, { useLayoutEffect, useEffect, useRef } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
 import "./NoteBookSection01.css";
 import hand from "../../../assets/Img-Section01/Page03/MANOS.png";
-import data from "../../../../trataSection01.json"
+import data from "../../../../trataSection01.json";
 
 function NoteBookSection01() {
   const page3 = data[0].visibilizacion_victimas.page3;
+  const textRef = useRef(null);
+  const imgRef = useRef(null);
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    // Animación para el texto
+    const textTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: textRef.current,
+        start: "top center", // Inicia la animación cuando el componente está en el centro de la vista
+        end: "bottom center", // Termina la animación cuando el componente está completamente fuera de la vista
+        scrub: true, // Hace que la animación sea suave mientras se desplaza
+        markers: true, // Muestra marcadores de ScrollTrigger para depuración
+      },
+    });
+
+    textTl.fromTo(
+      textRef.current,
+      {
+        x: "-100%", // Mueve el elemento hacia la izquierda al 100% de su ancho
+        opacity: 0, // Inicialmente establece la opacidad en 0 para que aparezca gradualmente
+      },
+      {
+        x: "0%", // Lleva el elemento a su posición original (0%)
+        opacity: 1, // Establece la opacidad en 1 para que sea completamente visible
+      }
+    );
+
+    // Animación para la imagen
+    const imgTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: imgRef.current,
+        start: "top center", // Inicia la animación cuando el componente está en el centro de la vista
+        end: "bottom center", // Termina la animación cuando el componente está completamente fuera de la vista
+        scrub: true, // Hace que la animación sea suave mientras se desplaza
+        markers: true, // Muestra marcadores de ScrollTrigger para depuración
+      },
+    });
+
+    imgTl.fromTo(
+      imgRef.current,
+      {
+        x: "100%", // Mueve el elemento hacia la derecha al 100% de su ancho
+        opacity: 0, // Inicialmente establece la opacidad en 0 para que aparezca gradualmente
+      },
+      {
+        x: "0%", // Lleva el elemento a su posición original (0%)
+        opacity: 1, // Establece la opacidad en 1 para que sea completamente visible
+      }
+    );
+  }, []);
+
 
   return (
     <div className="NoteBook__container">
       <div className="NoteBook">
-        <div className="NoteBook__texts">
-          <p
-            className="NoteBook__text1"
-            dangerouslySetInnerHTML={{ __html: page3.text1 }}
-          ></p>
+        <div className="NoteBook__texts" ref={textRef}>
+          <p className="NoteBook__text1" dangerouslySetInnerHTML={{ __html: page3.text1 }}></p>
           <h1 className="NoteBook__text2">{page3.text2}</h1>
           <h3 className="NoteBook__text3">{page3.text3}</h3>
         </div>
       </div>
-      <img className="img__hands" src={hand} />
+      <img className="img__hands" src={hand} alt="Hand" ref={imgRef} />
     </div>
   );
 }
