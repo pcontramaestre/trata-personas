6dd17159223da4c31356b07b79906a4cc72f3981
adrian4058 <adrian_2016_@outlook.es>
2023-08-24 19:00:32 -0300

    Popups añadidos

--- src/components/Slides/SliderSection01/SliderSection01.jsx
+++ src/components/Slides/SliderSection01/SliderSection01.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "./SliderSection01.css";
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const animateSlide = (slide, setSlidesViewed) => {
+  const tween = gsap.fromTo(slide, { x: "100%" }, { x: "0%", duration: 20 });
+
+  ScrollTrigger.create({
+    trigger: slide,
+    animation: tween,
+    start: "top bottom-=60%",
+    end: "top top",
+    toggleActions: "play none none none",
+    scrub: true,
+    onToggle: (self) => {
+      if (self.isActive) {
+        setSlidesViewed((prev) => prev + 1);
+      }
+    },
+  });
+};
 
 const SliderSection01 = ({ texts }) => {
+  const [slidesViewed, setSlidesViewed] = useState(0);
+  const [enableScroll, setEnableScroll] = useState(false);
+
+  useEffect(() => {
+    gsap.utils.toArray(".slider-container").forEach((slide) =>
+      animateSlide(slide, setSlidesViewed)
+    );
+  }, []);
+
+  useEffect(() => {
+    if (slidesViewed === texts.length) {
+      setEnableScroll(true);
+    }
+  }, [slidesViewed, texts]);
+
+  useEffect(() => {
+    if (enableScroll) {
+      // Enable page scrolling when all slides have been viewed
+      gsap.to(window, { duration: 0.1, scrollTo: "max" });
+      document.body.style.overflow = "auto"; // Restore scrolling
+    } else {
+      document.body.style.overflow = "hidden"; // Disable scrolling
+    }
+  }, [enableScroll]);
+
   const settings = {
     dots: false,
     infinite: true,
@@ -11,8 +58,7 @@ const SliderSection01 = ({ texts }) => {
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
-    autoplay: true,
-    autoplaySpeed: 3000,
+    autoplay: false,
   };
 
   return (
