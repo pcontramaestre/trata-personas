6fba876f2b0034908d09f77beac9a59b033c6ec1
Troy Pernía <troy00pernia@gmail.com>
2023-08-29 12:21:12 -0400

    change components/NoteBook: add useLayoutEffect for activated animation with scroll (line 16-182) and useEffect for to do the animation (line 30-167)

--- src/components/NoteBook/NoteBook.jsx
+++ src/components/NoteBook/NoteBook.jsx
@@ -1,4 +1,4 @@
-import { useState, useLayoutEffect, useEffect } from 'react'
+import { useState, useLayoutEffect, useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
@@ -6,7 +6,6 @@ import style from './NoteBook.module.css'
 
 import whiteSheet from '../../assets/NoteBook/whiteSheet.png'
 import whiteSheetShort from '../../assets/NoteBook/whiteSheetShort.png'
-// import { TimelineMax } from 'gsap/gsap-core'
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -28,39 +27,159 @@ function convertAngle (input) {
 function NoteBook ({ noteBook, topSection }) {
   if (!noteBook) return
 
-  const [texts, setTexts] = useState({})
+  const [texts, setTexts] = useState({
+    paragraph: noteBook.text.paragraph.map(() => ''),
+    title: noteBook.text.title.map(() => ''),
+    footer: ['']
+  })
+  const [currentText, setCurrentText] = useState({
+    paragraph: '',
+    title: '',
+    footer: ''
+  })
+  const [currentIndex, setCurrentIndex] = useState({
+    paragraph: 0,
+    title: 0,
+    footer: 0
+  })
+  const [activeAnimation, setActiveAnimation] = useState({
+    paragraph: false,
+    title: false,
+    footer: false
+  })
 
+  const listOrderOfText = useRef(listTextToWrite())
+  const indexOrder = useRef({
+    paragraph: 0,
+    title: 0
+  })
+
+  // Animation paragraph
   useEffect(() => {
-    if (noteBook) {
-      setTexts(() => ({
-        paragraph: noteBook.text.paragraph.map(() => ''),
-        title: noteBook.text.title.map(() => ''),
-        footer: ''
+    if (activeAnimation.paragraph && (currentIndex.paragraph < listOrderOfText.current.paragraph[indexOrder.current.paragraph].content.length)) {
+      const timeout = setTimeout(() => {
+        setCurrentText(prevText => ({
+          ...prevText,
+          paragraph: prevText.paragraph + listOrderOfText.current.paragraph[indexOrder.current.paragraph].content[currentIndex.paragraph]
+        }))
+        setCurrentIndex(prevIndex => ({
+          ...prevIndex,
+          paragraph: prevIndex.paragraph + 1
+        }))
+        const newTexts = {
+          ...texts
+        }
+        newTexts.paragraph[indexOrder.current.paragraph] = currentText.paragraph + listOrderOfText.current.paragraph[indexOrder.current.paragraph].content[currentIndex.paragraph]
+        setTexts(newTexts)
+      }, 10)
+      return () => { clearTimeout(timeout) }
+    } else if (activeAnimation.paragraph && (currentIndex.paragraph === listOrderOfText.current.paragraph[indexOrder.current.paragraph].content.length)) {
+      setCurrentText(prevText => ({
+        ...prevText,
+        paragraph: ''
       }))
+      setCurrentIndex(prevText => ({
+        ...prevText,
+        paragraph: 0
+      }))
+      indexOrder.current.paragraph = indexOrder.current.paragraph + 1
+      if (indexOrder.current.paragraph < listOrderOfText.current.paragraph.length) {
+        for (let i = 0; i < texts.paragraph.length; i++) {
+          if (!texts.paragraph[i].length) {
+            indexOrder.current.paragraph = i
+            break
+          }
+        }
+      } else {
+        setActiveAnimation(prevActivated => ({
+          ...prevActivated,
+          paragraph: !prevActivated.paragraph
+        }))
+      }
     }
-  }, [noteBook])
+  }, [currentText.paragraph, activeAnimation.paragraph])
+
+  // Animation title
+  useEffect(() => {
+    if (activeAnimation.title && (currentIndex.title < listOrderOfText.current.title[indexOrder.current.title].content.length)) {
+      const timeout = setTimeout(() => {
+        setCurrentText(prevText => ({
+          ...prevText,
+          title: prevText.title + listOrderOfText.current.title[indexOrder.current.title].content[currentIndex.title]
+        }))
+        setCurrentIndex(prevIndex => ({
+          ...prevIndex,
+          title: prevIndex.title + 1
+        }))
+        const newTexts = {
+          ...texts
+        }
+        newTexts.title[indexOrder.current.title] = currentText.title + listOrderOfText.current.title[indexOrder.current.title].content[currentIndex.title]
+        setTexts(newTexts)
+      }, 10)
+      return () => { clearTimeout(timeout) }
+    } else if (activeAnimation.title && (currentIndex.title === listOrderOfText.current.title[indexOrder.current.title].content.length)) {
+      setCurrentText(prevText => ({
+        ...prevText,
+        title: ''
+      }))
+      setCurrentIndex(prevText => ({
+        ...prevText,
+        title: 0
+      }))
+      indexOrder.current.title = indexOrder.current.title + 1
+      if (indexOrder.current.title < listOrderOfText.current.title.length) {
+        for (let i = 0; i < texts.title.length; i++) {
+          if (!texts.title[i].length) {
+            indexOrder.current.title = i
+            break
+          }
+        }
+      } else {
+        setActiveAnimation(prevActivated => ({
+          ...prevActivated,
+          title: !prevActivated.title
+        }))
+      }
+    }
+  }, [currentText.title, activeAnimation.title])
+
+  // Animation footer
+  useEffect(() => {
+    if (activeAnimation.footer && (currentIndex.footer < noteBook.text.footer.content.length)) {
+      const timeout = setTimeout(() => {
+        setCurrentText(prevText => ({
+          ...prevText,
+          footer: prevText.footer + noteBook.text.footer.content[currentIndex.footer]
+        }))
+        setCurrentIndex(prevIndex => ({
+          ...prevIndex,
+          footer: prevIndex.footer + 1
+        }))
+        const newTexts = {
+          ...texts
+        }
+        newTexts.footer[0] = currentText.footer + noteBook.text.footer.content[currentIndex.footer]
+        setTexts(newTexts)
+      }, 10)
+      return () => { clearTimeout(timeout) }
+    }
+  }, [currentText.footer, activeAnimation.footer])
 
   useLayoutEffect(() => {
     const containerAnimation = document.getElementsByName('noteBook' + noteBook.section)[0]
-    const elementsToMove = document.getElementsByName('noteBookText' + noteBook.section)
-    const arrayElementsToMove = Array.from(elementsToMove)
-    // console.log('elementsToMove:', elementsToMove)
 
     const ctx = gsap.context(() => {
-      // const animations = arrayElementsToMove.map(text => {
-      //   const animation = gsap.from(text, {
-      //     opacity: 0,
-      //     duration: 5
-      //   })
-      //   return animation
-      // })
-
       ScrollTrigger.create({
         trigger: containerAnimation,
-        markers: true,
-        start: 'top top',
+        // markers: true,
+        start: 'top 90%',
         onEnter: () => {
-          console.log('hola mundo')
+          setActiveAnimation({
+            paragraph: true,
+            title: true,
+            footer: true
+          })
         }
       })
     })
@@ -77,7 +196,7 @@ function NoteBook ({ noteBook, topSection }) {
     transform: `rotateZ(${-Number(noteBook.rotation.split('deg')[0])}deg)`
   }
 
-  const contentStyle = noteBook.text.paragraph.map(paragraph => ({
+  const contentStyle = noteBook.text.paragraph.sort((a, b) => a.positionContent - b.positionContent).map(paragraph => ({
     ...noteBook.text.paragraphStyles,
     content: '',
     width: convertSize(paragraph.width),
@@ -88,7 +207,7 @@ function NoteBook ({ noteBook, topSection }) {
     lineHeight: convertSize(noteBook.text.paragraphStyles.lineHeight)
   }))
 
-  const titleStyle = noteBook.text.title.map(title => ({
+  const titleStyle = noteBook.text.title.sort((a, b) => a.positionContent - b.positionContent).map(title => ({
     ...noteBook.text.titleStyles,
     width: convertSize(title.width),
     height: convertSize(title.height),
@@ -107,38 +226,22 @@ function NoteBook ({ noteBook, topSection }) {
     fontSize: convertSize(noteBook.text.footer.fontSize)
   }
 
-  function animationWriteText () {
+  function listTextToWrite () {
     const objectTexts = {
-      paragraph: noteBook.text.paragraph.map(content => content.content),
-      title: noteBook.text.title.map(title => title.content),
-      footer: noteBook.text.footer.content
+      paragraph: noteBook.text.paragraph.map(content => content),
+      title: noteBook.text.title.map(title => title)
     }
-    setTexts((texts) => ({
-      ...texts,
-      footer: objectTexts.footer
-    }))
+    objectTexts.paragraph.sort((a, b) => a.positionContent - b.positionContent)
+    objectTexts.title.sort((a, b) => a.positionContent - b.positionContent)
+
+    return objectTexts
   }
-
-  // function write (text) {
-  //   const arrayCharacters = text.split('')
-
-  // }
 
   return (
     <article name={'noteBook' + noteBook.section} className={style.NoteBook} style={whiteSheetStyle}>
-      {/* {
-          noteBook.text.paragraph.map((paragraph, index) => (
-            <p name={'noteBookText' + noteBook.section} className={style.NoteBookText} style={contentStyle[index]} dangerouslySetInnerHTML={{ __html: paragraph.content }} key={index} />
-          ))
-        }
       {
-          noteBook.text.title.map((title, index) => (
-            <h1 name={'noteBookText' + noteBook.section} className={style.NoteBookText} style={titleStyle[index]} key={index}>{title.content}</h1>
-          ))
-        } */}
-      {
-          noteBook.text.paragraph.map((paragraph, index) => (
-            <p name={'noteBookText' + noteBook.section} className={style.NoteBookText} style={contentStyle[index]} dangerouslySetInnerHTML={{ __html: texts && texts.paragraph ? texts.paragraph[index] : '' }} key={index} />
+          texts.paragraph.map((paragraph, index) => (
+            <p name={'noteBookText' + noteBook.section} className={style.NoteBookText} style={contentStyle[index]} dangerouslySetInnerHTML={{ __html: paragraph }} key={index} />
           ))
         }
       {
@@ -146,9 +249,7 @@ function NoteBook ({ noteBook, topSection }) {
             <h1 name={'noteBookText' + noteBook.section} className={style.NoteBookText} style={titleStyle[index]} key={index}>{texts && texts.title ? texts.title[index] : ''}</h1>
           ))
         }
-      {/* <footer name={'noteBookText' + noteBook.section} className={style.NoteBookText} style={footerStyle}>{noteBook.text.footer.content}</footer> */}
       <footer name={'noteBookText' + noteBook.section} className={style.NoteBookText} style={footerStyle}>{texts && texts.footer ? texts.footer : ''}</footer>
-      <button onClick={animationWriteText}>Agregar palabras</button>
     </article>
   )
 }
