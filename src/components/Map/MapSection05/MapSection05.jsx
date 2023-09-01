f0a3601b6fc7711060b2bc41e3094cf0d2e091cb
Jessica Grajales <fjessica28-11@hotmail.com>
2023-09-01 01:10:37 -0500

    1. Se termina mapa 5.

--- src/components/Map/MapSection05/MapSection05.jsx
+++ src/components/Map/MapSection05/MapSection05.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
-import { ReactComponent as Map } from "../../../assets/Section5/mapa/mapa3.svg"; // Cambio de "map" a "Map"
+import { useEffect, useState } from "react";
+import { ReactComponent as Map } from "../../../assets/Section5/mapa/mapa3.svg";
 import { ReactComponent as ArrowMX} from '../../../assets/Section5/mapa/MEXICO_Mexico.svg'
 import { ReactComponent as ArrowGT} from '../../../assets/Section5/mapa/GUATEMALA_Guatemala.svg'
 import { ReactComponent as ArrowBZ} from '../../../assets/Section5/mapa/BELICE_Belice.svg'
@@ -9,32 +9,24 @@ import { ReactComponent as ArrowCR} from '../../../assets/Section5/mapa/COSTARIC
 import { ReactComponent as ArrowPA} from '../../../assets/Section5/mapa/PANAMA_Panama.svg'
 import { ReactComponent as ArrowDO} from '../../../assets/Section5/mapa/REPUBLICADOMINICANA.svg'
 import "./MapSection05.css";
-// import arrowMX from '../../../assets/Section5/mapa/MEXICO_Mexico.svg'
-// import arrowGT from '../../../assets/Section5/mapa/GUATEMALA_Guatemala.svg'
-// import arrowBZ from '../../../assets/Section5/mapa/BELICE_Belice.svg'
-// import arrowSV from '../../../assets/Section5/mapa/ELSALVADOR_El.svg'
-// import arrowHN from '../../../assets/Section5/mapa/HONDURAS_Honduras.svg'
-// import arrowCR from '../../../assets/Section5/mapa/COSTARICA_Costa.svg'
-// import arrowPA from '../../../assets/Section5/mapa/PANAMA_Panama.svg'
-// import arrowDO from '../../../assets/Section5/mapa/REPUBLICADOMINICANA.svg'
 
 const arrayCountries = ["MX", "GT", "BZ", "SV", "HN", "CR", "PA", "DO", "NI"];
-// const countriesImage = [
-//   arrowMX,
-//   arrowGT,
-//   arrowBZ,
-//   arrowSV,
-//   arrowHN,
-//   arrowCR,
-//   arrowPA,
-//   arrowDO
-// ]
 
 const numberImages = {
 
 }
 
 function MapSection05() {
+  const [hoveredCountry, setHoveredCountry] = useState(null);
+
+  const handleCountryMouseEnter = (country) => {
+    setHoveredCountry(country);
+  };
+
+  const handleCountryMouseLeave = () => {
+    setHoveredCountry(null);
+  };
+
   useEffect(() => {
     const mapImages = arrayCountries.map((image, index) => {
       return document.querySelector('#mapImg' + (index + 1))
@@ -49,9 +41,10 @@ function MapSection05() {
           mapImages[index].style.display = "block"
           country.style.filter = 'brightness(0) saturate(100%) invert(87%) sepia(49%) saturate(3050%) hue-rotate(289deg) brightness(87%) contrast(95%)'
         });
-        // country.addEventListener("mouseleave", () => {
-        //   mapImages[index].style.display = ""
-        // });
+        country.addEventListener("mouseleave", () => {
+          mapImages[index].style.display = "none"; 
+          country.style.filter = '';
+        });
       }
     });
 
@@ -81,14 +74,6 @@ function MapSection05() {
       <ArrowCR id="mapImg6" />
       <ArrowPA id="mapImg7" />
       <ArrowDO id="mapImg8" />
-      {/* <img id="mapImg1" src={arrowMX} alt="" />
-      <img id="mapImg2" src={arrowGT} alt="" />
-      <img id="mapImg3" src={arrowBZ} alt="" />
-      <img id="mapImg4" src={arrowSV} alt="" />
-      <img id="mapImg5" src={arrowHN} alt="" />
-      <img id="mapImg6" src={arrowCR} alt="" />
-      <img id="mapImg7" src={arrowPA} alt="" />
-      <img id="mapImg8" src={arrowDO} alt="" /> */}
     </div>
   );
 }
