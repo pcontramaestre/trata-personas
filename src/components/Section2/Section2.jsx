8338c5c0d8d6df84d754d72c8573e096a8d7d270
adrian4058 <adrian_2016_@outlook.es>
2023-09-09 14:18:47 -0300

    animacion section 02

--- src/components/Section2/Section2.jsx
+++ src/components/Section2/Section2.jsx
@@ -13,16 +13,69 @@ import PracticasEsclavitud from "./Esclavitud/PracticasEsclavitud/PracticasEscla
 import ActividadesIlicitas from "./ActividadesIlicitas/ActividadesIlicitas";
 import Organos from "./Organos/Organos";
 import BoxInfoSection02 from "../BigInfoBox/BigInfoBoxSection02/BoxInfoSection02";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 function Section2() {
   const page1 = data[0].finalidades.page1;
+  const imagenMenTrembling = gsap.to(".page2__imagemen", {
+    x: "+=10", // Cambia el valor según la intensidad que desees para el temblor
+    repeat: -1, // Repite la animación indefinidamente
+    yoyo: true, // Hace que la animación vuelva a su posición original en cada repetición
+    ease: "power1.inOut", // Easing para suavizar el temblor
+  });
+  ScrollTrigger.create({
+    trigger: ".page2__imagemen", // Selector del elemento que desencadenará la animación
+    start: "top center", // Puedes ajustar esta configuración según tus necesidades
+    end: "bottom center",
+    animation: imagenMenTrembling,
+  });
+
+  const botas = document.querySelector(".page2__botas");
+  const sombrero = document.querySelector(".page2__sombreromen");
+
+  const caidaBotas = gsap.from(botas, {
+    y: -100, // La distancia desde la parte superior de la página
+    opacity: 0, // Inicialmente, las botas no son visibles
+    duration: 1, // Duración de la animación en segundos
+    ease: "power4.out", // Easing para suavizar la caída
+  });
+
+  const caidaSombrero = gsap.from(sombrero, {
+    y: -100, // La distancia desde la parte superior de la página
+    opacity: 0, // Inicialmente, el sombrero no es visible
+    duration: 1, // Duración de la animación en segundos
+    ease: "power4.out", // Easing para suavizar la caída
+  });
+  ScrollTrigger.create({
+    trigger: ".page2content", // Selector del elemento que desencadenará la animación
+    start: "top", // Puedes ajustar esta configuración según tus necesidades
+    end: "center",
+    onEnter: () => {
+      // Cuando el elemento es visible, inicia las animaciones de caída
+      caidaBotas.play();
+      caidaSombrero.play();
+    },
+    onLeave: () => {
+      // Cuando el elemento sale de la vista, pausa las animaciones (opcional)
+      caidaBotas.pause();
+      caidaSombrero.pause();
+    },
+    onEnterBack: () => {
+      // Cuando se regresa al elemento, continúa las animaciones (opcional)
+      caidaBotas.play();
+      caidaSombrero.play();
+    },
+  });
+
   return (
     <section id="section2" className="Section02">
       <div className="page2content">
         <div className="page2__imagesbackground">
-          <div className="page2__imagemen"/>
-          <div className="page2__sombreromen"/>
-          <div className="page2__botas"/>
+          <div className="page2__imagemen" />
+          <div className="page2__sombreromen" />
+          <div className="page2__botas" />
         </div>
         <div className="page2">
           <h1 className="page2__title">{page1.text1}</h1>
