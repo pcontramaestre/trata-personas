50968332dd6d1840a826af64315987979f6f815f
adrian4058 <adrian_2016_@outlook.es>
2023-09-15 13:58:31 -0300

    VideoCorett añadido

--- src/components/Section8/VideoCorett/VideoCorett.jsx
+++ src/components/Section8/VideoCorett/VideoCorett.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import data from "../../../../trataSection08.json";
-import playbutton from "../../../assets/Section8/playbutton.png";
+import videoCorett from "../../Video/VideoCorett.mp4";
 import "./VideoCorett.css";
 
 const VideoCorett = () => {
   const page5 = data[0].creditos.page5;
   return (
     <div className="video__corett">
-      <div className="videocorett__play">
-        <h1 className="videocorett__title">{page5.title}</h1>
-        <div className="videocorett__button">
-          <img className="videocorett__img" src={playbutton} />
-        </div>
-      </div>
+      <h1 className="videocorett__title">{page5.title}</h1>
+      <video controls className="videocorett__button">
+        <source src={videoCorett} type="video/mp4" />
+      </video>
       <div className="videocorett__info">
         <div className="videocorett__info-texts">
           <div className="info__separador" />
