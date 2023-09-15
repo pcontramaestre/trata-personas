d8e5e57dab5e07ed2012c6f0a51de9cc5a251382
Troy Pernía <troy00pernia@gmail.com>
2023-09-15 12:34:48 -0400

    change components/Section2: add props "handleVideosPlay" (line 25, 52)

--- src/components/Section2/Section2.jsx
+++ src/components/Section2/Section2.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./Section2.css";
 import data from "../../../trataSection02.json";
 import SliderSection02 from "../Slides/SliderSection02/SliderSection02";
@@ -21,7 +22,7 @@ import InfoConsentimiento from "../Section1/InfoConsentimiento/InfoConsentimient
 
 gsap.registerPlugin(ScrollTrigger);
 
-function Section2() {
+function Section2({handleVideosPlay}) {
   const page1 = data[0].finalidades.page1;
 
   return (
@@ -48,7 +49,7 @@ function Section2() {
       <SmallInfoSection02 />
       <GraphicFinalidades />
       <ExplotacionSexual />
-      <ExplotacionLaboral />
+      <ExplotacionLaboral handleVideosPlay={handleVideosPlay} />
       <MedicinaForzada />
       <Adopcion />
       <Matrimonio />
